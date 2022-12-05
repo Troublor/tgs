ea3eafdd45bc2cda6e3378e3e54a075d97ae7fe6
William Aaron Cheung <troublor@live.com>
2022-12-05 18:26:45 +0800

    :hammer: release scripts

---
+++ scripts/release.zsh
@@ -0,0 +1,34 @@
+#! /usr/bin/env zsh
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <major|minor|patch|prerelease>"
+    exit 1
+fi
+
+DIR=${0:a:h}
+ROOT=${DIR:h}
+
+cmds=(jq yq git)
+
+# shellcheck disable=SC2128
+for cmd in $cmds; do
+    if ! command -v "$cmd" >/dev/null 2>&1; then
+        echo "Command $cmd not found. Please install it and try again."
+        exit 1
+    fi
+done
+
+# bump version
+yarn version "$1"
+v=$(jq -r .version "$ROOT"/package.json)
+
+# update docker-compose.yaml
+yq -i ".services.tgs.image = \"troublor/tgs:$v\"" "$ROOT"/docker-compose.yaml
+
+# git commit
+git add "$ROOT"/package.json "$ROOT"/docker-compose.yaml
+git commit -m "chore: release $v"
+git tag "v$v"
+
+# prepare next cycle
+yarn version prerelease
